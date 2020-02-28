a955a3f218294682a9aad91505a8a6ef16ec9f79
justapixel <rhuan.dev@gmail.com>
2020-02-28 17:42:17 -0300

    feat: add file TimeSheet

---
+++ src/components/TimeSheet.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sheet">
+      <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeSheet',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+
+</style>
